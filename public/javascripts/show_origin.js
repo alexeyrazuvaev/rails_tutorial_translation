4c0f58acdcb10a0a7f237681d3e4f631952c3ef6
gafar <r125644@gmail.com>
2013-03-18 00:47:15 +0800

    Refactor _view_source_script partial.

---
+++ public/javascripts/show_origin.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+  $("a").click(function (event) {
+    event.stopPropagation();
+  });
+
+  $("p").toggle(
+    function() {
+      $("p").css("background-color","");
+      $('div.origin').remove();
+
+      var version = window.location.search.split('=')[1]
+      var chapter = window.location.pathname.split('/')[2]
+      var index = $("p").index(this);
+      var p_for_load = '/origin' + version + '/' + chapter + '_fragment.html p:eq(' + index + ')';
+
+      $(this).css("background-color","#fffacd");
+      $(this).after (function() { return ('<div class="origin"> </div>');});
+      $("div.origin").load(p_for_load);
+
+    },
+    function(){
+      $('div.origin').remove();
+      $(this).css("background-color","");
+    }
+  );
+});
+
