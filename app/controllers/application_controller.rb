71e834ca810c4d28f955d05d7595680b42a338ab
gafar <r125644@gmail.com>
2013-03-26 03:06:56 +0800

    Setup a redirect from www.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  before_filter :ensure_domain
+
+
   private
 
     # Use the Heroku Varnish cache.
@@ -11,4 +14,13 @@ class ApplicationController < ActionController::Base
       end
     end
 
+
+    APP_DOMAIN = 'railstutorial.ru'
+
+    def ensure_domain
+      if request.env['HTTP_HOST'] != APP_DOMAIN && ENV["RAILS_ENV"] != 'development'
+        # HTTP 301 is a "permanent" redirect
+        redirect_to "http://#{APP_DOMAIN}", :status => 301
+      end
+    end
 end
